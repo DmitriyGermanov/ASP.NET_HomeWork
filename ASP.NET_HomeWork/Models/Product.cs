b3819a1c5224408ef92be6af8ed79051f4dd58bc
Dmitriy Germanov <seltis.gtn@gmail.com>
2024-08-05 20:53:07 +0300

    product discription added

---
+++ ASP.NET_HomeWork/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace ASP.NET_HomeWork.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int? Cost { get; set; }
+        public string? Description { get; set; }
+    }
+}
